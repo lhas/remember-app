5a484d4c41c57a681ad322caad989657a6c1a466
luizhrqas <luizhrqas@gmail.com>
2014-01-01 23:53:27 -0200

    you now can destroy insights

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -1,4 +1,5 @@
 class InsightsController < ApplicationController
+  before_action :set_insight, only: [:edit, :update, :destroy]
 
   def create
     insight = Insight.new(insight_params)
@@ -10,11 +11,38 @@ class InsightsController < ApplicationController
     respond_to do |format|
       if insight.save
         format.html { redirect_to root_path, notice: 'Your insight was successfully created.' }
+      else
+        format.html { redirect_to root_path }
       end
     end
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @insight.update(insight_params)
+        format.html { redirect_to root_path, notice: 'Your insight was successfully updated.' }
+      else
+        format.html { redirect_to root_path }
+      end
+    end
+  end
+
+  def destroy
+    @insight.destroy
+    
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'Your insight was successfully deleted.' }
+    end
+  end
+
   private
+
+    def set_insight
+      @insight = current_user.insights.find(params[:id])
+    end
 
     def insight_params
       params.require(:insight).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 RememberApp::Application.routes.draw do
+  # Root
   root 'pages#index'
   
+  # Devise
   devise_for :users
   
+  # Contact Page
   get 'contact', to: 'pages#contact', as: 'contact'
 
+  # Insights
   post 'insights/create', to: 'insights#create', as: 'create_insight'
+  get 'insights/edit/:id', to: 'insights#edit', as: 'edit_insight'
+  patch 'insights/update/:id', to: 'insights#update', as: 'update_insight'
+  get 'insights/destroy/:id', to: 'insights#destroy', as: 'destroy_insight'
 end
